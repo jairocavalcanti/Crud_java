1acd3baf3bb2b090e678db7f673f1c08b8fe6bba
Jairo <jairoregiscavalcanti@gmail.com>
2023-10-01 01:46:35 -0300

    Crud_Finalizado

--- src/main/java/com/crud/crud/Database/RepositorioContato.java
+++ src/main/java/com/crud/crud/Database/RepositorioContato.java
@@ -2,6 +2,8 @@ package com.crud.crud.Database;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.crud.crud.entidade.Contato;
+
 public interface RepositorioContato extends JpaRepository<Contato,Long> {
-    
+
 }
