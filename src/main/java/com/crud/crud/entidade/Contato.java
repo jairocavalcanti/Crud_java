e22d94325b9d7183c3547172b45fdebfbed344cd
Jairo <jairoregiscavalcanti@gmail.com>
2023-09-30 23:41:57 -0300

    Entidade_Contato_Inserida

---
+++ src/main/java/com/crud/crud/entidade/Contato.java
@@ -0,0 +1,11 @@
+package com.crud.crud.entidade;
+
+public class Contato {
+
+    private Long id;
+    private String nome;
+    private String telefone;
+    private String email;
+    private String urlAvatar;
+    
+}
