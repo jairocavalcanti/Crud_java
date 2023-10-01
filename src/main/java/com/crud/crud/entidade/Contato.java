afc2ec28dd9993478355cc0adef49f0164ed113c
Jairo <jairoregiscavalcanti@gmail.com>
2023-09-30 23:43:23 -0300

    Entidade_Contato_alterada

--- src/main/java/com/crud/crud/entidade/Contato.java
+++ src/main/java/com/crud/crud/entidade/Contato.java
@@ -7,5 +7,40 @@ public class Contato {
     private String telefone;
     private String email;
     private String urlAvatar;
+   
+   
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getTelefone() {
+        return telefone;
+    }
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getUrlAvatar() {
+        return urlAvatar;
+    }
+    public void setUrlAvatar(String urlAvatar) {
+        this.urlAvatar = urlAvatar;
+    }
     
+
+    
+
 }
