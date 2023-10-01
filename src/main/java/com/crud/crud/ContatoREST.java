da5881d5491d8339520843a39e11f9ee4d7426b5
Jairo <jairoregiscavalcanti@gmail.com>
2023-10-01 01:17:33 -0300

    ContatoREST_mapeado

--- src/main/java/com/crud/crud/ContatoREST.java
+++ src/main/java/com/crud/crud/ContatoREST.java
@@ -3,32 +3,47 @@ package com.crud.crud;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.crud.crud.Database.RepositorioContato;
 import com.crud.crud.entidade.Contato;
 
+@RestController
+@RequestMapping("/contato")
 public class ContatoREST {
     
     @Autowired
     private RepositorioContato repositorio;
 
+   @GetMapping
    public List<Contato>listar(){
      return repositorio.findAll();
    }
 
-   public void salvar(Contato contato){
+   @PostMapping
+   //"@requestbody" faz com que o spring converta os dados de JSON para um objeto java
+   public void salvar(@RequestBody Contato contato){
      repositorio.save(contato);
    }
 
-   public void alterar(Contato contato){
+
+   @PutMapping
+   public void alterar(@RequestBody Contato contato){
     if(contato.getId()>0){
      repositorio.save(contato);
     }
    }
 
-    public void excluir(Contato contato){
+   @DeleteMapping
+    public void excluir(@RequestBody Contato contato){
       repositorio.delete(contato);
     }
 
-  
+
 }
