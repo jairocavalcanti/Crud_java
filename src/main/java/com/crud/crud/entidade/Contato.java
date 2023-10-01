61d5f81ed13357fb347addcaf9a1bf46bd3f0033
Jairo <jairoregiscavalcanti@gmail.com>
2023-09-30 23:51:06 -0300

    Entidade_Contato_mapeada

--- src/main/java/com/crud/crud/entidade/Contato.java
+++ src/main/java/com/crud/crud/entidade/Contato.java
@@ -1,10 +1,22 @@
 package com.crud.crud.entidade;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Contato {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(nullable = false)
     private String nome;
+    @Column(nullable = false)
     private String telefone;
+    @Column(nullable = false)
     private String email;
     private String urlAvatar;
    
